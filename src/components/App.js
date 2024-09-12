035690cb1d02601c08b606d668411fb0f88fd458
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2024-09-12 13:07:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,16 @@
 import { useState } from 'react';
 import React from "react";
+import Tab from './tab.jsx
 import './../styles/App.css';
 
 const App = () => {
-   const [data,setdta]=useState('Tab1');
-  function fxn(e){
-    setdta(e.target.textContent);
-    console.log(e.target.textContent)
+  
 
   }
-  
   return (
-    <div>
-        {/* Do not remove the main div */}
-    <ul>
-      <li onClick={fxn}>Tab1</li>
-      <li onClick={fxn}>Tab2</li>
-       <li onClick={fxn}>Tab3</li>
-    </ul>
-    <p> this is the content for {data}. </p>
-
-
-    
-    </div>
+    <>
+    <Tab/>
+    </>
   )
 }
 
